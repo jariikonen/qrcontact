efa15b156e78856aa368c0cb934b30e61380ec0d
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2025-06-18 09:13:56 +0300

    test: add eslint-plugin-testing-library and add scripts for testing in watch mode

--- packages/frontend/eslint.config.mjs
+++ packages/frontend/eslint.config.mjs
@@ -5,9 +5,10 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import testingLibrary from 'eslint-plugin-testing-library';
 
+// https://typescript-eslint.io/packages/typescript-eslint/#config
 export default tseslint.config(
-  { ignores: ['eslint.config.mjs', 'dist'] },
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
@@ -15,15 +16,22 @@ export default tseslint.config(
   reactRefresh.configs.recommended,
   jsxA11y.flatConfigs.recommended,
   eslintConfigPrettier,
+  { ignores: ['eslint.config.mjs', 'dist'] },
   {
+    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
+      parser: tseslint.parser,
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
     plugins: {
+      tseslint,
       react,
+      reactHooks,
+      reactRefresh,
+      jsxA11y,
     },
     settings: {
       react: {
@@ -43,6 +51,13 @@ export default tseslint.config(
         },
       ],
       'react/prop-types': 'off',
+    },
+  },
+  {
+    files: ['**/*.test.ts', '**/*.test.tsx'],
+    plugins: { testingLibrary },
+    rules: {
+      ...testingLibrary.configs['flat/dom'].rules,
     },
   }
 );

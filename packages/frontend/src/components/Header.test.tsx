59935d9621961e6a88cd3ad0db32ae1329e00e70
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2025-06-24 10:03:05 +0300

    refactor: add top level description to Header.test.tsx

--- packages/frontend/src/components/Header.test.tsx
+++ packages/frontend/src/components/Header.test.tsx
@@ -25,132 +25,134 @@ function mediaQueryMatches(query: string, width: number): boolean {
   return width >= min && width <= max;
 }
 
-describe('screen size md', () => {
-  beforeEach(() => {
-    window.matchMedia = vi.fn(createMatchMedia(1024));
+describe('Header', () => {
+  describe('screen size md', () => {
+    beforeEach(() => {
+      window.matchMedia = vi.fn(createMatchMedia(1024));
+    });
+
+    test('renders heading', () => {
+      const menuOption = 0;
+      const setMenuOption = vitest.fn();
+      render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+
+      const heading = screen.getByRole('heading', { level: 1 });
+      expect(heading).toBeInTheDocument();
+      expect(heading).toBeVisible();
+      expect(heading).toHaveTextContent('QRContact');
+    });
+
+    test('renders tabs', () => {
+      const menuOption = 0;
+      const setMenuOption = vitest.fn();
+      render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+
+      const tabs = screen.getAllByRole('tab');
+      expect(tabs).toHaveLength(2);
+      expect(tabs[0]).toHaveTextContent(
+        'Create a QR code from your contact information'
+      );
+      expect(tabs[1]).toHaveTextContent(
+        'Create a contact page and link to it with a QR code'
+      );
+    });
+
+    test('tab 0 is selected, when menuOption is 0', () => {
+      const menuOption = 0;
+      const otherOption = 1;
+      const setMenuOption = vitest.fn();
+      render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+
+      const tabs = screen.getAllByRole('tab');
+      expect(tabs).toHaveLength(2);
+      expect(tabs[menuOption]).toHaveAttribute('aria-selected', 'true');
+      expect(tabs[otherOption]).toHaveAttribute('aria-selected', 'false');
+    });
+
+    test('tab 1 is selected, when menuOption is 1', () => {
+      const menuOption = 1;
+      const otherOption = 0;
+      const setMenuOption = vitest.fn();
+      render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+
+      const tabs = screen.getAllByRole('tab');
+      expect(tabs).toHaveLength(2);
+      expect(tabs[menuOption]).toHaveAttribute('aria-selected', 'true');
+      expect(tabs[otherOption]).toHaveAttribute('aria-selected', 'false');
+    });
   });
 
-  test('renders heading', () => {
-    const menuOption = 0;
-    const setMenuOption = vitest.fn();
-    render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+  describe('screen size sm', () => {
+    beforeEach(() => {
+      window.matchMedia = vi.fn(createMatchMedia(375));
+    });
 
-    const heading = screen.getByRole('heading', { level: 1 });
-    expect(heading).toBeInTheDocument();
-    expect(heading).toBeVisible();
-    expect(heading).toHaveTextContent('QRContact');
-  });
+    test('renders heading', () => {
+      const menuOption = 0;
+      const setMenuOption = vitest.fn();
+      render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
 
-  test('renders tabs', () => {
-    const menuOption = 0;
-    const setMenuOption = vitest.fn();
-    render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+      const heading = screen.getByRole('heading', { level: 1 });
+      expect(heading).toBeInTheDocument();
+      expect(heading).toBeVisible();
+      expect(heading).toHaveTextContent('QRContact');
+    });
 
-    const tabs = screen.getAllByRole('tab');
-    expect(tabs).toHaveLength(2);
-    expect(tabs[0]).toHaveTextContent(
-      'Create a QR code from your contact information'
-    );
-    expect(tabs[1]).toHaveTextContent(
-      'Create a contact page and link to it with a QR code'
-    );
-  });
+    test('renders menu button', () => {
+      const menuOption = 0;
+      const setMenuOption = vitest.fn();
+      render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
 
-  test('tab 0 is selected, when menuOption is 0', () => {
-    const menuOption = 0;
-    const otherOption = 1;
-    const setMenuOption = vitest.fn();
-    render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+      const menuButton = screen.getByLabelText('menu');
+      expect(menuButton).toBeInTheDocument();
+      expect(menuButton).toBeVisible();
+      expect(menuButton).toHaveRole('button');
+    });
 
-    const tabs = screen.getAllByRole('tab');
-    expect(tabs).toHaveLength(2);
-    expect(tabs[menuOption]).toHaveAttribute('aria-selected', 'true');
-    expect(tabs[otherOption]).toHaveAttribute('aria-selected', 'false');
-  });
+    test('renders menu items', async () => {
+      const menuOption = 0;
+      const setMenuOption = vitest.fn();
+      render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
 
-  test('tab 1 is selected, when menuOption is 1', () => {
-    const menuOption = 1;
-    const otherOption = 0;
-    const setMenuOption = vitest.fn();
-    render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+      const menuButton = screen.getByLabelText('menu');
+      fireEvent.click(menuButton);
 
-    const tabs = screen.getAllByRole('tab');
-    expect(tabs).toHaveLength(2);
-    expect(tabs[menuOption]).toHaveAttribute('aria-selected', 'true');
-    expect(tabs[otherOption]).toHaveAttribute('aria-selected', 'false');
-  });
-});
+      const buttonStatic = await screen.findByTestId('menu-button-static');
+      const buttonDynamic = await screen.findByTestId('menu-button-dynamic');
+      expect(buttonStatic).toBeInTheDocument();
+      expect(buttonDynamic).toBeInTheDocument();
+      expect(buttonStatic).toBeVisible();
+      expect(buttonDynamic).toBeVisible();
+      expect(buttonStatic).toHaveRole('button');
+      expect(buttonDynamic).toHaveRole('button');
+    });
 
-describe('screen size sm', () => {
-  beforeEach(() => {
-    window.matchMedia = vi.fn(createMatchMedia(375));
-  });
+    test('setMenuOption() gets called with 0 when menu-button-static is clicked', async () => {
+      const menuOption = 0;
+      const setMenuOption = vitest.fn();
+      render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
 
-  test('renders heading', () => {
-    const menuOption = 0;
-    const setMenuOption = vitest.fn();
-    render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+      const menuButton = screen.getByLabelText('menu');
+      fireEvent.click(menuButton);
 
-    const heading = screen.getByRole('heading', { level: 1 });
-    expect(heading).toBeInTheDocument();
-    expect(heading).toBeVisible();
-    expect(heading).toHaveTextContent('QRContact');
-  });
+      const buttonStatic = await screen.findByTestId('menu-button-static');
+      fireEvent.click(buttonStatic);
 
-  test('renders menu button', () => {
-    const menuOption = 0;
-    const setMenuOption = vitest.fn();
-    render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+      expect(setMenuOption).toHaveBeenCalledWith(0);
+    });
 
-    const menuButton = screen.getByLabelText('menu');
-    expect(menuButton).toBeInTheDocument();
-    expect(menuButton).toBeVisible();
-    expect(menuButton).toHaveRole('button');
-  });
+    test('setMenuOption() gets called with 1 when menu-button-dynamic is clicked', async () => {
+      const menuOption = 0;
+      const setMenuOption = vitest.fn();
+      render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
 
-  test('renders menu items', async () => {
-    const menuOption = 0;
-    const setMenuOption = vitest.fn();
-    render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
+      const menuButton = screen.getByLabelText('menu');
+      fireEvent.click(menuButton);
 
-    const menuButton = screen.getByLabelText('menu');
-    fireEvent.click(menuButton);
+      const buttonDynamic = await screen.findByTestId('menu-button-dynamic');
+      fireEvent.click(buttonDynamic);
 
-    const buttonStatic = await screen.findByTestId('menu-button-static');
-    const buttonDynamic = await screen.findByTestId('menu-button-dynamic');
-    expect(buttonStatic).toBeInTheDocument();
-    expect(buttonDynamic).toBeInTheDocument();
-    expect(buttonStatic).toBeVisible();
-    expect(buttonDynamic).toBeVisible();
-    expect(buttonStatic).toHaveRole('button');
-    expect(buttonDynamic).toHaveRole('button');
-  });
-
-  test('setMenuOption() gets called with 0 when menu-button-static is clicked', async () => {
-    const menuOption = 0;
-    const setMenuOption = vitest.fn();
-    render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
-
-    const menuButton = screen.getByLabelText('menu');
-    fireEvent.click(menuButton);
-
-    const buttonStatic = await screen.findByTestId('menu-button-static');
-    fireEvent.click(buttonStatic);
-
-    expect(setMenuOption).toHaveBeenCalledWith(0);
-  });
-
-  test('setMenuOption() gets called with 1 when menu-button-dynamic is clicked', async () => {
-    const menuOption = 0;
-    const setMenuOption = vitest.fn();
-    render(<Header menuOption={menuOption} setMenuOption={setMenuOption} />);
-
-    const menuButton = screen.getByLabelText('menu');
-    fireEvent.click(menuButton);
-
-    const buttonDynamic = await screen.findByTestId('menu-button-dynamic');
-    fireEvent.click(buttonDynamic);
-
-    expect(setMenuOption).toHaveBeenCalledWith(1);
+      expect(setMenuOption).toHaveBeenCalledWith(1);
+    });
   });
 });
